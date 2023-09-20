5feef9147bc90b014a0f007a3974aa58a3d9d05e
Shoaib Farooq <m.shoaib1050@gmail.com>
2023-09-20 13:57:43 +0500

    Categories

--- lib/category_screen/item_details.dart
+++ lib/category_screen/item_details.dart
@@ -1,4 +1,5 @@
 import 'package:online_shoping/consts/consts.dart';
+import 'package:online_shoping/widgets_common/our_button.dart';
 
 class ItemDetils extends StatelessWidget {
   final String? title;
@@ -7,7 +8,7 @@ class ItemDetils extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: lightGrey,
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: title!.text.color(darkFontGrey).fontFamily(bold).make(),
         actions: [
@@ -15,7 +16,76 @@ class ItemDetils extends StatelessWidget {
           IconButton(onPressed: (){}, icon: const Icon(Icons.favorite_outline)),
         ],
       ),
-      body: Container(),
+      body: Column(
+        children: [
+          Expanded(child: Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [VxSwiper.builder(
+                    autoPlay: true,
+                    height: 250,
+                    aspectRatio: 16/9,
+                    itemCount: 3,
+                    itemBuilder: (context,index){
+                  return Image.asset(imgFc5,width: double.infinity,fit: BoxFit.cover);
+                    }),
+                  10.heightBox,
+                  title!.text.size(16).color(darkFontGrey).fontFamily(bold).make(),
+                  10.heightBox,
+                  VxRating(onRatingUpdate: (value){},
+                  normalColor: textfieldGrey,
+                  selectionColor: golden,
+                  count: 5, size: 25, stepInt: true,
+                  ),
+                  10.heightBox,
+                  "\$30,000".text.color(redColor).fontFamily(bold).size(18).make(),
+                  10.heightBox,
+                  Row(children: [
+                    Expanded(child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                      "Seller".text.white.fontFamily(semibold).make(),
+                        5.heightBox,
+                      "In House Brand".text.fontFamily(semibold).color(darkFontGrey).size(16).make()],),),
+                const CircleAvatar(backgroundColor: Colors.white,
+                  child: Icon(Icons.message_rounded, color: darkFontGrey,),
+                )],
+                  ).box.height(60).padding(const EdgeInsets.symmetric(horizontal: 16)).color(textfieldGrey).make(),
+                  20.heightBox,
+                  Column(children: [
+                    Row(
+                      children: [
+                        SizedBox(width: 100,
+                            child: "Color:".text.color(textfieldGrey).make()
+                    ),
+                    Row(children: List.generate(3, (index) => VxBox().size(40, 40).roundedFull.color(Vx.randomOpaqueColor)
+                        .margin(const EdgeInsets.symmetric(horizontal: 4)).make(),
+                    ),)
+                  ],
+                  ).box.padding(const EdgeInsets.all(8)).make(),
+                    Row(
+                      children: [
+                        SizedBox(width: 100,
+                            child: "Color:".text.color(textfieldGrey).make()
+                        ),
+                        Row(children: List.generate(3, (index) => VxBox().size(40, 40).roundedFull.color(Vx.randomOpaqueColor)
+                            .margin(const EdgeInsets.symmetric(horizontal: 4)).make(),
+                        ),)
+                      ],
+                    ).box.padding(const EdgeInsets.all(8)).make()
+                  ],).box.white.shadowSm.make(),
+                ],
+              ),
+            )
+          ),
+          ),
+          SizedBox(width: double.infinity,height: 60,
+          child: ourButton(color: redColor,onPress: (){}, textColor: whiteColor, title: "Add to Cart"),),
+        ],
+      )
     );
   }
 }
